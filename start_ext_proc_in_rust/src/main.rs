f5f118e058aced36f75ef4dae40db746c84dfa41
Jurek Michal-AMJ018 <Michal.Jurek@motorolasolutions.com>
2024-06-20 18:13:44 +0200

    spawn() methong on command used

--- start_ext_proc_in_rust/src/main.rs
+++ start_ext_proc_in_rust/src/main.rs
@@ -1,12 +1,29 @@
-use std::process::Command;
+use std::io::Write;
+use std::process::{Command, Stdio};
 
 fn main() {
-    let output = Command::new("echo")
-        .arg("Hello, my world!")
-        .output()
-        .expect("Failed to executed command");
+    // Call to 'blocking' output()
+    let mut date_cmd = Command::new("date");
 
-
-    let out_str = String::from_utf8(output.stdout.to_vec()).expect("Cannot convert Vec<u8> into String");
+    let out_str = String::from_utf8(date_cmd.output().unwrap().stdout.to_vec()).expect("Cannot convert Vec<u8> into String");
     println!("out: {}", out_str);
+    let out_str = String::from_utf8(date_cmd.arg("--utc").output().unwrap().stdout.to_vec()).expect("Cannot convert Vec<u8> into String");
+    println!("out: {}", out_str);
+
+    // Spawning int separate thread ('non-blocking' spawn)
+    let mut rev_cmd = Command::new("rev")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let mut std_in = rev_cmd.stdin.take().unwrap();
+    //let mut std_out = rev_cmd.stdout.take().unwrap();
+
+    std::thread::spawn(move || {
+        std_in.write_all("Hello World".as_bytes()).unwrap();
+    });
+
+    let output = rev_cmd.wait_with_output().unwrap();
+    println!("out from rev: {}", String::from_utf8_lossy(&output.stdout));
 }
