a9ed6e6daba7c32b1481014eeea30f181276d3e3
Michal Jurek <jurmcc@gmail.com>
2024-09-28 21:39:28 +0200

    Rust drawing (now more c calls from Rust)

--- rust_color_effects/src/main.rs
+++ rust_color_effects/src/main.rs
@@ -1,40 +1,86 @@
 extern crate ncurses;
-extern crate core;
 
 use ncurses::*;
-use std::ffi::c_int;
-use std::ffi::c_uint;
+use std::{thread, time};
+use rand::prelude::*;
 
-#[repr(C)]
-pub struct DropStruct {
-    x: c_int,
-    y: c_int,
+#[derive(Copy, Clone)]
+struct Coord {
+    y: i32,
+    x: i32,
+}
+
+struct DropStruct {
+    tail: Vec<Coord>,
     live: bool,
-}
-
-#[repr(C)]
-pub struct DropsStruct {
-    len: c_int,
-    drops: *const DropStruct,
-}
-
-extern "C" {
-    fn drops_main(drops: *mut DropsStruct);
-    fn dump_drop_data(y: c_int, drop: &DropStruct);
-    fn dump_drops_data(len: c_int, drops: *const DropStruct);
-    fn dump_drops_data2(s: *const DropsStruct);
-    fn dump_array(drops: &DropsStruct);
 }
 
 fn drops_init(len: usize) -> Vec<DropStruct> {
     let mut retval = Vec::with_capacity(len);
 
-    for i in 1..len {
-        let d = DropStruct { x: i as i32, y: 0, live: false };
+    for i in 0..len {
+        let d = DropStruct { tail: vec![Coord{y: 5, x: i as i32}], live: false };
         retval.push(d);
     }
 
     retval
+}
+
+fn spawn_drops(drops: &mut Vec<DropStruct>) {
+    let len = drops.len();
+
+    let empty_slots = drops.iter().filter(|item| item.live == false).count();
+
+    if 0 == empty_slots {
+        return;
+    }
+
+    let mut rng = thread_rng();
+    let toss = rng.gen_range(0..empty_slots);
+    mvprintw(1, 1, &format!("len: {}, toss: {}", len, toss));
+    refresh();
+
+    let tossed = drops.iter_mut().filter(|item| item.live == false).nth(toss).unwrap();
+    tossed.live = true;
+
+    //for (uint32_t i = 0; i < drops->len; i++) {
+    //    if (false == drops->drops[i].live) {
+    //        if (toss == 0) {
+    //            drops->drops[i].live = true;
+    //            break;
+    //        }
+    //        --toss;
+    //    }
+    //}
+
+}
+
+fn kick_drops(drops: &mut Vec<DropStruct>) {
+    for d in drops.iter_mut() {
+        let mut new_cell: Option<Coord> = None;
+        if d.tail.len() < 7 {
+            new_cell = Some(d.tail.iter().last().copied().unwrap());
+        }
+        for c in d.tail.iter_mut() {
+            c.y += 1;
+        }
+
+        match new_cell {
+            Some(nc) => d.tail.push(nc),
+            _ => (),
+        }
+    }
+}
+
+fn print_drops(drops: &Vec<DropStruct>) {
+    for d in drops.iter().filter(|drop| drop.live) {
+        for (i, cell) in d.tail.iter().enumerate() {
+            let col = i as i16 + 1;
+            attron(COLOR_PAIR(col as i16));
+            mvaddch(cell.y, cell.x, '.' as u32);
+            attroff(COLOR_PAIR(col as i16));
+        }
+    }
 }
 
 fn main() {
@@ -45,42 +91,29 @@ fn main() {
     noecho();
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
 
-    /////
-    let d1 = DropStruct{x: 5, y: 7, live: true};
-    let d2 = DropStruct{x: 10, y: 14, live: false};
-    let d3 = DropStruct{x: 5, y: 7, live: true};
-    let d4 = DropStruct{x: 10, y: 14, live: false};
-    unsafe {
-        dump_drop_data(1, &d1);
-        dump_drop_data(2, &d2);
-    }
-    let array: [DropStruct; 2] = [d1, d2];
-    let array2: [DropStruct; 2] = [d3, d4];
-    let mut y = 0;
-    for item in array.iter() {
-        let s = String::from("x: ");
-        mvprintw(32+y, 20, format!("[Rust] x: {}, y: {}, live: {}", item.x, item.y, item.live).as_str()).unwrap();
-        y += 1;
-    }
-    unsafe {
-        dump_drops_data(2, array.as_ptr());
-    }
-    let drops = DropsStruct{
-        len: 2,
-        drops: array2.as_ptr()};
-    unsafe {
-        dump_drops_data2(&drops);
-        std::mem::forget(drops);
-    }
-    /////
+    init_pair(1, COLOR_BLACK, COLOR_RED);
+    init_pair(2, COLOR_BLACK, COLOR_GREEN);
+    init_pair(3, COLOR_BLACK, COLOR_YELLOW);
+    init_pair(4, COLOR_BLACK, COLOR_BLUE);
+    init_pair(5, COLOR_BLACK, COLOR_MAGENTA);
+    init_pair(6, COLOR_BLACK, COLOR_CYAN);
+    init_pair(7, COLOR_BLACK, COLOR_WHITE);
+    init_pair(8, COLOR_WHITE, COLOR_BLACK);
 
-    let new_drops = drops_init(COLS() as usize);
-    let mut drops = DropsStruct {
-        len: new_drops.len() as c_int,
-        drops: new_drops.as_ptr(),
-    };
-    unsafe {
-        drops_main(&mut drops);
+    let mut drops = drops_init(COLS() as usize); // TODO: should be cols
+
+    let sec = time::Duration::from_secs(1);
+    let mut ch = getch();
+    let q = 113 as i32;
+    while ch != q {
+        kick_drops(&mut drops);
+        clear();
+        print_drops(&drops);
+        spawn_drops(&mut drops);
+        refresh();
+
+        thread::sleep(sec);
+        ch = getch();
     }
 
     endwin();
